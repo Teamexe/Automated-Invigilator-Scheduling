a62678a2fcae1e616a0913de0af168553b9395b8
Amit Chambial <chambialamit@gmail.com>
2018-04-01 00:22:16 +0530

    Added Teacher

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,7 +1,27 @@
 class TeachersController < ApplicationController
   before_action :authenticate_teacher!,:is_member
   def index
+    @duties=ExamDuty.where(teacher:current_teacher)
   end
+  def change_password
+    
+  end
+  def change
+    old_password=params[:teacher][:password_old]
+    new_password=params[:teacher][:password_new]
+    # puts old_password,new_password
+    if(current_teacher.valid_password?(old_password))
+      puts "Valid"
+      if @current_teacher.update_column(:encrypted_password, ::BCrypt::Password.create("#{new_password}"))
+        sign_in(@current_teacher, :bypass => true)
+    redirect_to '/teachers/index',notice: "Password Changed Succesfully!"
+        
+      end
+    else
+      redirect_to '/teachers/change_password',alert: 'Password does not match.'
+    end
+  end
+
   private
   def is_member
     if current_teacher.admin? == true
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'main#index'
   get 'main/index'
   get 'teachers/index'
-
+  get 'teachers/change_password'
+  post 'teachers/change'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
